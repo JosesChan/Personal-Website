fae207dcefd3b2f519db159067b4e928f8ec0bcc
Deimus <joses.chan@gmail.com>
2022-11-21 17:29:29 +0000

    Reworking backend

--- back_end/server.js
+++ back_end/server.js
@@ -1,13 +1,23 @@
-import express from "express"
-import cors from "cors"
-import webData from "./api/webData.route.js"
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config({ path: "./config.env" });
 
-const app = express()
+// use port set in config.env or 5000
+const port = process.env.PORT || 5000;
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
+app.use(require("./routes/record"));
 
-app.use("/api/v1/web_data", webData)
-app.use("*", (req,res) => res.status(404).json({error:"not found"}))
-
-export default app
+// get driver connection
+const dbo = require("./db/conn");
+ 
+app.listen(port, () => {
+  // perform a database connection when server starts
+  dbo.connectToServer(function (err) {
+    if (err) console.error(err);
+ 
+  });
+  console.log(`Server is running on port: ${port}`);
+});
